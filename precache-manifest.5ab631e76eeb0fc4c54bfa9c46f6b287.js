c355e45548cf719fbc57ed32b71b74f39166fa4c
Gijaye Real <gijayecodes@gmail.com>
2020-09-29 22:33:58 +0800

    Updates

---
+++ precache-manifest.5ab631e76eeb0fc4c54bfa9c46f6b287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65655757ee1b7faab738cc1aa56b1dbe",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "5c5f9620113cd2ca94d9",
+    "url": "/todoList/static/css/main.8bd5a762.chunk.css"
+  },
+  {
+    "revision": "a79bb07ee6268562c71e",
+    "url": "/todoList/static/js/2.7616cd3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoList/static/js/2.7616cd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5f9620113cd2ca94d9",
+    "url": "/todoList/static/js/main.38004c16.chunk.js"
+  },
+  {
+    "revision": "1c8d98858a1240a8c21a",
+    "url": "/todoList/static/js/runtime-main.6164c0d8.js"
+  }
+]);
